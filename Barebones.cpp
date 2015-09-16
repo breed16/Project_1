eb69e21018a0b69603203e26554d5f90b690bb49
Rafer Cooley <rafecoolz@gmail.com>
2015-09-16 15:36:57 -0600

    Create Barebones.cpp

---
+++ Barebones.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+int main(){
+
+//int list[]
+//double list[]
+int k;
+int i=0;
+int input;
+
+while(cin >> input){//tests if input is an int/double/etc..
+	//read in the number and sort it into list[]
+	if(x != 0){
+		sort(x);
+	}else{
+		break;
+	}
+}
+
+int diff[list.Size()-1];
+
+for(int j=0; j<list[].Size(); j++){
+	diff[j]=k-list[j];
+}
+
+int smallBegin;
+int largeEnd;
+for(int k=0; k<diff[].Size(); k++){
+	smallBegin=list[0];
+	largeEnd=list[size-1];
+	if((smallBegin+largeEnd))
+}
+
+}
+
+
+public *int sort(int x){
+	//some sort of sort ;-0
+}
